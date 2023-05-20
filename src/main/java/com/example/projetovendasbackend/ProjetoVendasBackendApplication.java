8193de1ed43b759eb4ee07d7b9102568aa1fa334
alexferreiraofc <alex.ferreiraofc@gmail.com>
2023-05-20 09:37:14 -0300

    first commit

---
+++ src/main/java/com/example/projetovendasbackend/ProjetoVendasBackendApplication.java
@@ -0,0 +1,13 @@
+package com.example.projetovendasbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjetoVendasBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ProjetoVendasBackendApplication.class, args);
+    }
+
+}
